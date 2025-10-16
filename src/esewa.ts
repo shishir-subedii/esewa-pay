b4b79fe045ae9687f7fa5ff7fbb2d29b0a6e047a
Shishir <shishir16subedi@gmail.com>
2025-10-16 13:12:55 +0545

    feat(esewa): customize intiate payment, verifypayment and transaction status

--- src/esewa.ts
+++ src/esewa.ts
@@ -1,4 +1,4 @@
-import axios from "axios";
+import axios, { AxiosResponse } from "axios";
 import { generateHmacSha256 } from "./utils/crypto";
 import { API_URLS, STATUS_URLS } from "./config";
 import type { EsewaConfig, PaymentParams, StatusResponse, VerifyResponse } from "./types";
@@ -19,7 +19,7 @@ export class EsewaClient {
         return generateHmacSha256(data, this.config.secretKey);
     }
 
-    async initiatePayment(params: PaymentParams) {
+    async initiatePayment(params: PaymentParams): Promise<string> {
         const signature = this.generateSignature(params.total_amount, params.transaction_uuid, this.config.productCode);
 
         const payload = {
@@ -33,23 +33,36 @@ export class EsewaClient {
             success_url: this.config.successUrl,
             failure_url: this.config.failureUrl,
             signed_field_names: "total_amount,transaction_uuid,product_code",
-            signature,
+            signature: signature
         };
-
-        return {
+        const paymentConfig = {
             url: this.baseUrl,
-            method: "POST",
-            payload,
-        };
+            data: payload,
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            responseHandler: (response: AxiosResponse) => response.request?.res?.responseUrl,
+        }
+
+        // Make payment request
+        const paymentReq = await axios.post(String(paymentConfig.url), paymentConfig.data, {
+            headers: paymentConfig.headers,
+        });
+
+        const paymentUrl = paymentConfig.responseHandler(paymentReq);
+        if(!paymentUrl) {
+            throw new Error("Failed to get payment URL");
+        }
+        return paymentUrl;
     }
 
-    async verifyTransaction(signature: string, response: VerifyResponse): Promise<boolean> {
-        const data = `transaction_code=${response.transaction_code},status=${response.status},total_amount=${response.total_amount},transaction_uuid=${response.transaction_uuid},product_code=${response.product_code},signed_field_names=${response.signed_field_names}`;
-        const generatedSignature = generateHmacSha256(data, this.config.secretKey);
-        return generatedSignature === signature;
+    async verifyPayment(data: string){
+        const decodedData = Buffer.from(data, 'base64').toString('utf-8');
+        const parsed = JSON.parse(decodedData) as VerifyResponse;
+        return parsed;
     }
 
-    async checkStatus(transaction_uuid: string, total_amount: string): Promise<StatusResponse> {
+    async getTransactionStatus(transaction_uuid: string, total_amount: string): Promise<StatusResponse> {
         const url = `${this.statusUrl}?product_code=${this.config.productCode}&total_amount=${total_amount}&transaction_uuid=${transaction_uuid}`;
         const { data } = await axios.get<StatusResponse>(url);
         return data;
